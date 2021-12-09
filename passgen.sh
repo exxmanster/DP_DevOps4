c16d26b775df6650de4946f2a392517873c42575
exxmanster <exxmanster@gmail.com>
2021-12-09 22:20:17 +0200

    Finish Nginx maintaining script

--- passgen.sh
+++ passgen.sh
@@ -60,3 +60,4 @@
 ### Sort for randomly mixing all the stuff, head is cut it out number of reqested chars(random in case flag is empty flag and awk is removing our seed)
 pass | sort -R | head -${LENGTH} | awk '{printf "%s",$1}'
  echo " "
+                                                                     
