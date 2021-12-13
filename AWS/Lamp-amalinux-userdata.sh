4438bdcd559ea06ec830f01506d121407a2c4abf
exxmanster <exxmanster@gmail.com>
2021-12-13 17:40:15 +0200

    Add a LAMP user script

--- AWS/Lamp-amalinux-userdata.sh
+++ AWS/Lamp-amalinux-userdata.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo yum update -y && \\
-sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2 && \\
-sudo yum install -y httpd mariadb-server php-mbstring php-xml && \\
+sudo yum update -y && 
+sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2 
+sudo yum install -y httpd mariadb-server php-mbstring php-xml
 sudo sudo usermod -aG apache ec2-user && sudo chown -R ec2-user:apache /var/www && \\
 sudo chmod 2775 /var/www && find /var/www -type d -exec sudo chmod 2775 {} \; && \\
 find /var/www -type f -exec sudo chmod 0664 {} \; && \\
@@ -11,6 +11,6 @@ cd /var/www/html && wget https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-
 mkdir phpMyAdmin && tar -xvzf phpMyAdmin-latest-all-languages.tar.gz -C phpMyAdmin --strip-components 1 && \\
 rm phpMyAdmin-latest-all-languages.tar.gz
 sleep 3
-sudo systemctl restart mariadb httpd php-fpm && 
+sudo systemctl restart mariadb httpd php-fpm 
 
 
