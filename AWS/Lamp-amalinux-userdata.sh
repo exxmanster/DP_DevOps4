574cf32bc7121c3abe3919302b318b5c70d8b8f3
exxmanster <exxmanster@gmail.com>
2021-12-15 16:27:00 +0200

    Add AWS Stuff

--- AWS/Lamp-amalinux-userdata.sh
+++ AWS/Lamp-amalinux-userdata.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo yum update -y && 
-sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2 
+sudo amazon-linux-extras install php7.2 
 sudo yum install -y httpd mariadb-server php-mbstring php-xml
 sudo sudo usermod -aG apache ec2-user && sudo chown -R ec2-user:apache /var/www && \\
 sudo chmod 2775 /var/www && find /var/www -type d -exec sudo chmod 2775 {} \; && \\
