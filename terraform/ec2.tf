85cad8c9373160aa680e69c913abec28823704a6
exxmanster <exxmanster@gmail.com>
2021-12-28 15:04:29 +0200

    Bastion fixes

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "web1" {
   ami           = "ami-05d34d340fb1d89e5"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.private1.id
-  key_name = aws_key_pair.ssh-key.key_name
+  key_name = aws_key_pair.ssh-key-from-bastion.key_name
   iam_instance_profile = "SSMRole"
   vpc_security_group_ids = [aws_security_group.allow_http.id, aws_security_group.allow_ssh.id]
   user_data = file("nginx-userdata.sh")
@@ -16,7 +16,7 @@ resource "aws_instance" "web2" {
   ami           = "ami-05d34d340fb1d89e5"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.private2.id
-  key_name = aws_key_pair.ssh-key.key_name
+  key_name = aws_key_pair.ssh-key-from-bastion.key_name
   iam_instance_profile = "SSMRole"
   vpc_security_group_ids = [aws_security_group.allow_http.id, aws_security_group.allow_ssh.id]
   user_data = file("nginx-userdata.sh")
@@ -29,7 +29,7 @@ resource "aws_instance" "db1" {
   ami           = "ami-05d34d340fb1d89e5"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.private1.id
-  key_name = aws_key_pair.ssh-key.key_name
+  key_name = aws_key_pair.ssh-key-from-bastion.key_name
   iam_instance_profile = "SSMRole"
   vpc_security_group_ids = [aws_security_group.allow_http.id, aws_security_group.allow_ssh.id]
   user_data = file("nginx-db-userdata.sh")
@@ -42,7 +42,7 @@ resource "aws_instance" "db2" {
   ami           = "ami-05d34d340fb1d89e5"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.private2.id
-  key_name = aws_key_pair.ssh-key.key_name
+  key_name = aws_key_pair.ssh-key-from-bastion.key_name
   iam_instance_profile = "SSMRole"
   vpc_security_group_ids = [aws_security_group.allow_http.id, aws_security_group.allow_ssh.id]
   user_data = file("nginx-db-userdata.sh")
@@ -55,10 +55,30 @@ resource "aws_instance" "bastion" {
   ami           = "ami-05d34d340fb1d89e5"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.public1.id
-  key_name = aws_key_pair.ssh-key.key_name
+  key_name = aws_key_pair.ssh-key-to-bastion.key_name
   iam_instance_profile = "SSMRole"
   vpc_security_group_ids = [aws_security_group.allow_http.id, aws_security_group.allow_ssh.id]
-  user_data = ""
+  
+  provisioner "file" {
+    source = "bastion_id"
+    destination = "/home/ec2-user/.ssh/id_rsa"
+
+  }
+  
+  connection {
+      type = "ssh"
+      user = "ec2-user"
+      host = "${aws_instance.bastion.public_ip}"
+  }
+
+provisioner "remote-exec" {    
+  inline = [
+    "sudo chmod 600 /home/ec2-user/.ssh/id_rsa",
+    "sudo yum update -y && sudo yum install -y python3-pip && pip3 install ansible"
+  ]
+
+
+}
   tags = {
     Name = "bastion"
    }
--- terraform/main.tf
+++ terraform/main.tf
@@ -10,9 +10,14 @@ terraform {
 provider "aws" {
   region = "eu-central-1"
 }
-resource "aws_key_pair" "ssh-key" {
-  key_name   = "ssh-key"
+resource "aws_key_pair" "ssh-key-to-bastion" {
+  key_name   = "to-bastion-key"
   public_key = file("id_rsa.pub")
+}
+
+resource "aws_key_pair" "ssh-key-from-bastion" {
+  key_name   = "from-bastion-key"
+  public_key = file("bastion_id.pub")
 }
 
 # output "lb_url" {  
