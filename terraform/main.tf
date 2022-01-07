52eb65a2fb08f14606b1d064936b029c01c5b4b2
exxmanster <exxmanster@gmail.com>
2022-01-08 00:43:54 +0200

    nginx

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,21 +25,21 @@ resource "aws_key_pair" "ssh-key-from-bastion" {
 #   value       = "http://${aws_alb.main.public_dns}/"
 #   }
 
-  output "bastion_pub_ip" {  
-  description = "Bastion Public IP"  
+output "bastion_pub_ip" {
+  description = "Bastion Public IP"
   value       = "ssh ec2-user@${aws_instance.bastion.public_ip}"
-  }
-
-  output "web_ips" {
-    description = "Web IP's"
-    value = "${aws_instance.web1.private_ip}, ${aws_instance.web2.private_ip}"
-  
 }
 
- output "db_ips" {
-    description = "DBs IP's"
-    value = "${aws_instance.db1.private_ip}, ${aws_instance.db2.private_ip}"
- }
+output "web_ips" {
+  description = "Web IP's"
+  value       = "${aws_instance.web1.private_ip}, ${aws_instance.web2.private_ip}"
+
+}
+
+output "db_ips" {
+  description = "DBs IP's"
+  value       = "${aws_instance.db1.private_ip}, ${aws_instance.db2.private_ip}"
+}
 
 #   output "priv_ip" {  
 #   description = "private IPs"  
