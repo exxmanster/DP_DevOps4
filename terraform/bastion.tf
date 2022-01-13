c32d2eda975332a26abbfc23a6faf2213807517b
exxmanster <exxmanster@gmail.com>
2022-01-13 15:48:13 +0200

    Chenge bastion provisioning to userdata

--- terraform/bastion.tf
+++ terraform/bastion.tf
@@ -5,6 +5,15 @@ resource "aws_instance" "bastion" {
   key_name               = aws_key_pair.ssh-key-to-bastion.key_name
   iam_instance_profile   = "SSMRole"
   vpc_security_group_ids = [module.vpc.http_sg, module.vpc.ssh_sg]
+  user_data = <<EOF
+#!/bin/bash
+sudo chmod 600 /home/ec2-user/.ssh/id_rsa
+sudo yum update -y && sudo yum install -y python3-pip && pip3 install ansible boto3
+sudo ansible-galaxy collection install amazon.aws
+sudo yum install -y git && git clone https://github.com/exxmanster/DP_DevOps4.git
+
+  EOF
+	
 
 
   # provisioner "file" {
